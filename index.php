840302ed6a33a5ca3757c655259c6e491a104840
Pavel-t34 <Paflik12@gmail.com>
2016-12-12 23:28:10 +0800

    Update and rename LICENSE.md to index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head></head>
+<body>
+  
+   <style>
+  body 
+     { 
+       background: url(gibdd.jpg) right bottom no-repeat;
+     }
+      a.button9 {
+  position: relative;
+  display: inline-block;
+  color: #0641dd;
+  font-weight: bold;
+  text-decoration: none;
+  text-shadow: rgba(255,255,255,.5) 1px 1px, rgba(100,100,100,.3) 3px 7px 3px;
+  user-select: none;
+  padding: 1em 2em;
+  outline: none;
+  border-radius: 3px / 100%;
+  background-image:
+   linear-gradient(45deg, rgba(31,53,250,.0) 30%, rgba(250,31,31,.8), rgba(31,53,250,.0) 70%),
+   linear-gradient(to right, rgba(31,53,250,1), rgba(31,53,250,0) 20%, rgba(255,255,255,0) 90%, rgba(255,255,255,.3)),
+   linear-gradient(to right, rgba(31,53,250,1), rgba(31,53,250,.9) 45%, rgba(125,125,125,.5)),
+   linear-gradient(to right, rgba(31,53,250,1), rgba(31,53,250,.9) 45%, rgba(125,125,125,.5)),
+   linear-gradient(to right, rgba(31,53,250,1), rgba(31,53,250,.9) 45%, rgba(223,190,170,.5)),
+   linear-gradient(to right, rgba(31,53,250,1), rgba(31,53,250,.9) 45%, rgba(223,190,170,.5));
+  background-repeat: no-repeat;
+  background-size: 200% 100%, auto, 100% 2px, 100% 2px, 100% 1px, 100% 1px;
+  background-position: 200% 0, 0 0, 0 0, 0 100%, 0 4px, 0 calc(100% - 4px);
+  box-shadow: rgba(0,0,0,.5) 3px 10px 10px -10px;
+}
+a.button9:hover {
+  transition: .8s linear;
+  background-position: -200% 0, 0 0, 0 0, 0 100%, 0 4px, 0 calc(100% - 4px);
+}
+a.button9:active {
+  top: 1px;
+}
+    </style>
+<body>
+  <table>
+   
+    <tr><td><a href="/secret.php" class="button9">Терминал регистрации</a></td></tr>
+    <tr><td><a href="/post.php" class="button9">Терминал постового</a></td></tr>
+  </table>
+</body>
+  
+</ht
