31ba8078509ef2d573b6ce44d909dc0d56221033
Pavel-t34 <Paflik12@gmail.com>
2016-12-12 23:33:45 +0800

    Add files via upload

---
+++ forms/regprot.php
@@ -0,0 +1,25 @@
+﻿<form action="<?= $url ?>" method="post">
+
+
+  <label>Дата</label><br>
+  <input type="text" name="date" value=""><br>
+  <br>
+  
+  <label>Нарушение</label><br>
+  <input type="text" name="violation" value=""><br>
+  <br>
+
+  <label>Водитель</label><br>
+  <input type="text" name="owner" value=""><br>
+  <br>
+  
+  <label>Автомобиль</label><br>
+  <input type="text" name="car" value=""><br>
+  <br>
+  
+  <label>Инспектор</label><br>
+  <input type="text" name="insp" value=""><br>
+  <br>
+  
+  <button type="submit">Сохранить</button>
+</fo
---
+++ forms/scanavto.php
@@ -0,0 +1,10 @@
+﻿<form action="../scanavto.php " method="post">
+
+
+  <label>Номер</label><br>
+  <input type="text" name="number" value=""><br>
+  <br>
+
+  
+  <button type="submit">Поиск</button>
+</fo
